4dc4d976930efc5c5a50f18607969385fda2813d
BRY402 <105960244+BRY402@users.noreply.github.com>
2022-09-10 16:38:48 -0300

    Create anti nls logging.lua

---
+++ scriptsoup/anti nls logging.lua
@@ -0,0 +1,65 @@
+-- this is very simple and probably bypasseable (with out of sandbox code) so if you for some reason use it and find a bypass pls tell me i beg u
+local http = game:GetService("HttpService")
+local __Locals = {}
+local onls = NLS
+local id = 0
+local extrasrc = [==[local rscript = script.Rscript.Value
+local ar = script:WaitForChild("ActionsRemote")
+local http = game:GetService("HttpService")
+local function getbans()
+local banlist = http:JSONDecode(ar:InvokeServer("GetBans"))
+return banlist
+end
+local ar = nil
+local http = nil]==]
+local reasons = {"Invalid Data type, table or nil expected"}
+local function format(str,name)
+local sct = {Loaded = false,
+src = str or "",
+Name = name or script.Name or "NLS|ID:"..id}
+return sct
+end
+local function NLS(src,parent,Data)
+local Data = Data or {}
+assert(typeof(Data) == "table","NLS was rejected, reason: "..reasons[1])
+id = id + 1
+local sct = format(src,Data.Name or nil)
+sct.Script = onls("",parent)
+sct.Script.Name = sct.Name
+local rsc = Instance.new("ObjectValue",sct.Script)
+rsc.Name = "Rscript"
+rsc.Value = Data.Rscript or script
+if sct.Script:IsA("LocalScript") and sct.Script:FindFirstChild("Source") and not sct.Loaded then
+sct.Loaded = true
+-- actions remote
+local ar = Instance.new("RemoteFunction",sct.Script)
+ar.Name = "ActionsRemote"
+ar.OnServerInvoke = function(at)
+if at == "GetBans" then
+local list = game:GetBans()
+for i,v in pairs(list) do
+if typeof(v) ~= "table" then
+list[i] = tostring(v)
+end
+end
+return http:JSONEncode(list)
+end
+end
+-- execution for nls
+coroutine.resume(coroutine.create(function()
+while task.wait() do
+sct.Script:FindFirstChild("Source").OnServerInvoke = function(plr)
+if plr == owner then
+return extrasrc.."\n"..src
+else
+print(plr.." attempted to log you")
+return "No tampering pls"
+end
+end
+end
+end))
+end
+table.insert(__Locals,sct)
+return sct.Script
+end
+return NLS,__Locals
