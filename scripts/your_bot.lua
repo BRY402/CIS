db3ad1d57a7b571649fa34fac9afa71fa8c28f72
BRY402 <105960244+BRY402@users.noreply.github.com>
2023-02-10 15:10:36 -0300

    Update your_bot.lua

--- scripts/your_bot.lua
+++ scripts/your_bot.lua
@@ -125,7 +125,7 @@ local function Load(API_KEY, name)
 						local unit = (PlayerHrp.Position - HumanoidRootPart.Position).Unit
 						local mag = (PlayerHrp.Position - HumanoidRootPart.Position).Magnitude
 						Humanoid:MoveTo(PlayerHrp.Position - unit * 4)
-						if mag >= 1.5 and mag <= 5 then
+						if mag >= 2 and mag <= 5 then
 							local x, y, z = CFrame.lookAt(HumanoidRootPart.Position, PlayerHrp.Position).Rotation:ToOrientation()
 							Character:PivotTo(CFrame.new(HumanoidRootPart.Position) * HumanoidRootPart.CFrame.Rotation:Lerp(CFrame.Angles(0, y, 0), .65))
 						end
