dd921022d4ea02e642f840304e35ea80dc75fb11
BRY402 <105960244+BRY402@users.noreply.github.com>
2023-04-05 17:29:58 -0300

    Update stuff/carter.lua

--- stuff/carter.lua
+++ stuff/carter.lua
@@ -1,7 +1,7 @@
 local HttpService = game:GetService("HttpService")
 local lib = loadstring(HttpService:GetAsync("https://github.com/BRY402/random-scripts/raw/main/stuff/lib.lua",true))()
 local bots = {}
-local carter = {new = function(api_key, scene, version)
+local carter = {new = function(api_key, version)
 	local storage = {
 		key = "",
 		scene = "Normal",
