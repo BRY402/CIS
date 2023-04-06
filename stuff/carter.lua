300369dec11e43ea9994835c38391c081987b944
BRY402 <105960244+BRY402@users.noreply.github.com>
2023-04-05 23:42:48 -0300

    Update stuff/carter.lua

--- stuff/carter.lua
+++ stuff/carter.lua
@@ -1,7 +1,8 @@
 local HttpService = game:GetService("HttpService")
 local lib = loadstring(HttpService:GetAsync("https://github.com/BRY402/random-scripts/raw/main/stuff/lib.lua",true))()
 local function getResponse(data)
-	local response = HttpService:RequestAsync({Url = storage.url,
+	local response = HttpService:RequestAsync({
+		Url = storage.url,
 		Method = "POST",
 		Headers = {["Content-Type"] = "application/json"},
 		Body = HttpService:JSONEncode(data)
