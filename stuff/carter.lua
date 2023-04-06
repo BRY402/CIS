f32afbce040515039eed9880065945789b619513
BRY402 <105960244+BRY402@users.noreply.github.com>
2023-04-06 00:43:37 -0300

    Update stuff/carter.lua

--- stuff/carter.lua
+++ stuff/carter.lua
@@ -32,15 +32,15 @@ local carter = {new = function(api_key, version_)
 			storage.scene = "Normal"
 			function bot:Send(msg, player)
 				assert(storage.CanChat,"Bot#"..tostring(table.find(bots, bot)).." is disabled")
-				local id = player and player.UserId or 0
+				local id = player and player.UserId or ""
 				if player and not table.find(storage.ids, id) then
-					table.insert(storage.ids,id)
+					table.insert(storage.ids, id)
 					ChatterEvent:AddChatter(player)
 				end
 				local outputData = getResponse("https://api.carterapi.com/v0/chat", {
 					api_key = storage.key,
 					query = msg,
-					uuid = id,
+					uuid = global and "" or id,
 					scene = storage.scene
 				})
 				local outputText = outputData.output.text
@@ -56,7 +56,7 @@ local carter = {new = function(api_key, version_)
 		V1 = function()
 			function bot:Send(msg, player)
 				assert(storage.CanChat,"Bot#"..tostring(table.find(bots, bot)).." is disabled")
-				local id = player and player.UserId or 0
+				local id = player and player.UserId or ""
 				if player and not table.find(storage.ids, id) then
 					table.insert(storage.ids,id)
 					ChatterEvent:AddChatter(player)
